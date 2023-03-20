2832bac2495abd468b6ca5750d91c434eef23c62
Lais <lais.salesms@gmail.com>
2023-03-20 14:43:08 -0300

    Correção Exercício 1

--- aula_04/src/aula_04/Exercicio1.java
+++ aula_04/src/aula_04/Exercicio1.java
@@ -8,6 +8,7 @@ public class Exercicio1 {
 		Scanner entrada = new Scanner(System.in);
 		int vetorInteiros[] = {1,4,2,6,8,3,7,9,5,10};
 		int numero;
+		boolean encontrou = false;
 		
 		System.out.println("Digite o número que deseja encontrar: ");
 		numero = entrada.nextInt();
@@ -15,7 +16,12 @@ public class Exercicio1 {
 		for (int i = 0; i < 10; i++) {
 			if (vetorInteiros[i] == numero) {
 				System.out.println("O número " + numero + " está na posição: " + i);
+				encontrou = true;
 			}
+		}
+		
+		if (encontrou == false) {
+			System.out.println("O número não foi encontrado!");
 		}
 		
 		entrada.close();
