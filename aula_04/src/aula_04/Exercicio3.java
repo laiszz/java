2efc70eebff22de573487a8312e714243007b11d
Lais <lais.salesms@gmail.com>
2023-03-20 15:18:07 -0300

    Adicionando comentários no Exercício 4

--- aula_04/src/aula_04/Exercicio3.java
+++ aula_04/src/aula_04/Exercicio3.java
@@ -10,6 +10,7 @@ public class Exercicio3 {
 		int[][] matrizNumerosEntrada = new int[3][3];
 		float somaPrincipal = 0, somaSecundaria = 0;
 		
+		// Entrada de dados
 		for (int i = 0; i < matrizNumerosEntrada.length; i++) {
 			for (int j = 0; j < matrizNumerosEntrada[i].length; j++) {
 				System.out.println("Digite o número da posição [" + i + "][" + j + "]: ");
@@ -19,8 +20,10 @@ public class Exercicio3 {
 		
 		System.out.println("Elementos da diagonal principal: ");
 		
+		// Achando os elementos da Diagonal Principal
 		for (int i = 0; i < matrizNumerosEntrada.length; i++) {
 			for (int j = 0; j < matrizNumerosEntrada[i].length; j++) {
+				// Se linha = coluna, faz parte da DP
 				if (i == j) {
 					System.out.println(matrizNumerosEntrada[i][j]);
 					
@@ -31,9 +34,11 @@ public class Exercicio3 {
 		
 		System.out.println("Elementos da diagonal secundária: ");
 		
+		// Achando os elementos da Diagonal Secundária
 		for (int i = 0; i < matrizNumerosEntrada.length; i++) {
 			for (int j = 0; j < matrizNumerosEntrada[i].length; j++) {
-				if (i+j == 2) {
+				// Se linha+coluna = numeroDeColunas-1, faz parte da DS
+				if (i+j == matrizNumerosEntrada[i].length - 1) {
 					System.out.println(matrizNumerosEntrada[i][j]);
 					
 					somaSecundaria += matrizNumerosEntrada[i][j];
